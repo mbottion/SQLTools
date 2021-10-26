46e236794ad0aae95938026fc9d51abdd323cc05
mbottion <84838458+mbottion@users.noreply.github.com>
2021-10-26 22:15:12 +0200

    Updated by runSQL.sh

--- genStatPrefsCompare.sql
+++ genStatPrefsCompare.sql
@@ -23,9 +23,9 @@ begin
 |      Generates a SQL SCript to by run on another DATABASE to compare init parameters :
 |
 |   Parameters :
-|       mode     : sceham name ("%" allowed)
-|       table    : table_name ("%" allowed)
-|       pref     : pref name ("%" allowed)
+|       mode     : Schema name ("%" allowed) : Default %
+|       table    : table_name ("%" allowed)  : Default %
+|       pref     : pref name ("%" allowed)   : Default %
 |       
 +---------------------------------------------------------------------------------------
        ');
@@ -54,6 +54,28 @@ column ord format 999 noprint
 column a format a400 
 column b format a400 newline
 set recsep off
+
+
+select
+         'Prompt                                                                          ' || chr(10) ||
+         'Prompt     Running preference comparison ....                                   ' || chr(10) ||
+         'Prompt     ==================================                                   ' || chr(10) ||
+         'Prompt                                                                          ' || chr(10) ||
+         'Prompt     If a lot of tables are selected, this script may run for             ' || chr(10) ||
+         'Prompt  several hours, please be patient ....                                   ' || chr(10) ||
+         'Prompt                                                                          ' || chr(10) ||
+         'col owner                    format a10                                         ' || chr(10) ||
+         'col table_name               format a30                                         ' || chr(10) ||
+         'col value                    format a50                                         ' || chr(10) ||
+         'col value_' || name || '     format a50                                         ' || chr(10) ||
+         'set newpage none feedback off                                                   ' || chr(10) ||
+         '' a
+  ,5    ord
+  ,null ord
+  ,null ord
+  ,null ord
+from v$database
+/
 
 with
   function get_or_comp_pref(s in varchar2,t in varchar2,p in varchar2,old_value in varchar2 default null) return varchar2 is
@@ -94,136 +116,47 @@ selected_tables as (
       and table_name like &tableName
 --      and table_name in ('RES_ANALYSE_RESSOURCE','RES_PERIODE_MANQUANTE','RES_RESSOURCE_RETENUE','PER_RES_BASE','PER_RES_BASE_ANALYSE','GPI_PRESTATION','GPI_DROIT','GPI_DROIT_RESANALYSE','PER_MAINTIEN_DROIT')
       and pref_name like &prefName    )
-select
-         'Prompt                                                                          ' || chr(10) ||
-         'Prompt     Running preference comparison ....                                   ' || chr(10) ||
-         'Prompt     ==================================                                   ' || chr(10) ||
-         'Prompt                                                                          ' || chr(10) ||
-         'Prompt     If a lot of tables are selected, this script may run for             ' || chr(10) ||
-         'Prompt  several hours, please be patient ....                                   ' || chr(10) ||
-         'Prompt                                                                          ' || chr(10) ||
-         'col owner                    format a10                                         ' || chr(10) ||
-         'col table_name               format a30                                         ' || chr(10) ||
-         'col value                    format a50                                         ' || chr(10) ||
-         'col value_' || name || '     format a50                                         ' || chr(10) ||
-         'with                                                                            ' || chr(10) ||
-         '  function get_or_comp_pref(s in varchar2, t in varchar2                        ' || chr(10) ||
-         '                           ,p in varchar2, old_value in varchar2 default null)  ' || chr(10) ||
-         '  return varchar2 is                                                            ' || chr(10) ||
-         '    v varchar2(100) ;                                                           ' || chr(10) ||
-         '  begin                                                                         ' || chr(10) ||
-         '--    begin                                                                     ' || chr(10) ||
-         '      v := dbms_stats.get_prefs(p,s,t) ;                                        ' || chr(10) ||
-         '    exception when others then                                                  ' || chr(10) ||
-         '      if   (sqlcode =-20001 )  then v := ''N/A'' ;                              ' || chr(10) ||
-         '      elsif (sqlcode =-20000 ) then v := ''*** Non Existant Table ***'' ;       ' || chr(10) ||
-         '      else                          raise ;                                     ' || chr(10) ||
-         '      end if ;                                                                  ' || chr(10) ||
-         '      return(v);                                                                ' || chr(10) ||
-         '--    end ;                                                                     ' || chr(10) ||
-         '--    if old_value is null then return (nvl(v,''NULL'')) ;                      ' || chr(10) ||
-         '--    else                                                                      ' || chr(10) ||
-         '--      if old_value <> v then return (''DIFFERENT'') ;                         ' || chr(10) ||
-         '--      else                   return (''SAME'');                               ' || chr(10) ||
-         '--      end if ;                                                                ' || chr(10) ||
-         '--    end if ;                                                                  ' || chr(10) ||
-         '  end ;                                                                         ' || chr(10) ||
-         '' a
-  ,5    ord
-  ,null ord
-  ,null ord
-  ,null ord
-from v$database
-UNION
-select 
-   '      select null Owner  ,null table_name  , null pref_name                  , null VALUE                                                                    , null VALUE_' || name || ' from dual where 1=2' a
-  ,10   ord 
-  ,null ord
-  ,null ord
-  ,null ord
-from v$database
-UNION
-select
-   'UNION select o,t,n,p1,p2 from ('
-  ,20
-  ,null
-  ,null
-  ,null
-from dual
-UNION
-select
-  '   select owner o,table_name t,pref_name n,get_or_comp_pref(owner,table_name,pref_name) p1,pref_value p2 from ('
-  ,30
-  ,null
-  ,null
-  ,null
-from dual
-UNION
-select
-  '   select * from ('
-  ,31
-  ,null
-  ,null
-  ,null
-from dual
-UNION
+
+
 select
    case 
-   when rownum=1           then '                          '  
-   when mod(rownum,500)=1  then '     ) UNION ALL (        ' 
-   else               '                          UNION ALL ' end || 
-       'select ''' || owner || ''' owner,'''  || table_name || ''' table_name,''' || 
+   when mod(rownum,500)=1  then 
+     case when rownum=1 then '' else '                   ))) where p1!=p2 order by 2,1 '|| chr(10) || '/' || chr(10) end || chr(10) ||
+     case when rownum>1 then 'set heading off' else '' end || chr(10) ||
+     'with                                                                            ' || chr(10) ||
+     '  function get_or_comp_pref(s in varchar2, t in varchar2                        ' || chr(10) ||
+     '                           ,p in varchar2, old_value in varchar2 default null)  ' || chr(10) ||
+     '  return varchar2 is                                                            ' || chr(10) ||
+     '    v varchar2(100) ;                                                           ' || chr(10) ||
+     '  begin                                                                         ' || chr(10) ||
+     '    begin                                                                       ' || chr(10) ||
+     '      v := dbms_stats.get_prefs(p,s,t) ;                                        ' || chr(10) ||
+     '    exception when others then                                                  ' || chr(10) ||
+     '      if   (sqlcode =-20001 )  then v := ''N/A'' ;                              ' || chr(10) ||
+     '      elsif (sqlcode =-20000 ) then v := ''*** Non Existant Table ***'' ;       ' || chr(10) ||
+     '      else                          raise ;                                     ' || chr(10) ||
+     '      end if ;                                                                  ' || chr(10) ||
+     '    end ;                                                                       ' || chr(10) ||
+     '    return(v);                                                                  ' || chr(10) ||
+     '  end ;                                                                         ' || chr(10) ||
+     '           select null Owner  ,null table_name  , null pref_name , null VALUE  , null VALUE_' || db.name || ' from dual where 1=2' || chr(10) ||
+     'UNION select o,t,n,p1,p2 from (' || chr(10) ||
+     '  select owner o,table_name t,pref_name n,get_or_comp_pref(owner,table_name,pref_name) p1,pref_value p2 from (' || chr(10) ||
+     '    select * from (          '
+   else               
+     '                   UNION ALL ' end || 
+     'select ''' || owner || ''' owner,'''  || table_name || ''' table_name,''' || 
        pref_name ||  ''' pref_name,'''  || get_or_comp_pref(owner,table_name,pref_name) || ''' pref_value from dual'
-  ,40
-  ,lpad(rownum,6,'0') || owner  
-  ,table_name
-  ,pref_name
-from selected_tables
+  ,40 ord
+  ,lpad(rownum,6,'0') || owner  ord
+  ,table_name ord
+  ,pref_name ord
+from selected_tables 
+join v$database db on (1=1)
 UNION
 select
-  '            )'
+  '                  ))) where p1!=p2 order by 2, 1' || chr(10) || '/' || chr(10)
   ,41
-  ,null
-  ,null
-  ,null
-from dual
-UNION
-select
-  '                      )'
-  ,50
-  ,null
-  ,null
-  ,null
-from dual
-UNION
-select
-  '              ) where p1!=p2'
-  ,51
-  ,null
-  ,null
-  ,null
-from dual
-/*UNION
-select
-  'UNION select '''||owner||''','''||table_name||''','''||pref_name||''',''*** Non Existant Table ***'','''||get_or_comp_pref(owner,table_name,pref_name) ||''' from dual '||
-     ' where not exists (select 1 from dba_tables where owner=''' || owner||''' and table_name=''' ||table_name || ''')' 
-  ,60
-  ,null
-  ,null
-  ,null
-from selected_tables */
-union
-select 
-  'order by 2,1'
-  ,70 ord 
-  ,null
-  ,null
-  ,null
-from dual
-union
-select 
-  '/'
-  ,71 ord 
   ,null
   ,null
   ,null
