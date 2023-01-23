48e4334c6857e8b85023978b68a9df4a64dd48a3
mbottion <84838458+mbottion@users.noreply.github.com>
2023-01-23 22:13:06 +0100

    Create hiddenParams.sql

---
+++ hiddenParams.sql
@@ -0,0 +1,19 @@
+set line 200
+ col PARAMETER format a30
+ col DESCRIPTION format a60
+ col SESSION_VALUE format a10
+ col INSTANCE_VALUE format a10
+ col NUM format 9999
+ 
+ SELECT a.ksppinm AS parameter,
+        a.INDX    as num,
+        a.ksppdesc AS description,
+        b.ksppstvl AS session_value,
+        c.ksppstvl AS instance_value
+ FROM   x$ksppi a,
+        x$ksppcv b,
+        x$ksppsv c
+ WHERE  a.indx = b.indx
+ AND    a.indx = c.indx
+ AND    a.ksppinm LIKE '/_%' ESCAPE '/'
+ ORDER BY a.ksppinm;
