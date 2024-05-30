ee09128586d10739f5a59e8de9e43d0427257178
mbottion <84838458+mbottion@users.noreply.github.com>
2024-05-30 17:45:48 +0200

    Update hangAnalyze.sh

--- hangAnalyze.sh
+++ hangAnalyze.sh
@@ -1,14 +1,46 @@
-if [ "$1" = "" ]
-then
-  echo "hangAnalyse.sh PDB"
+usage()
+{
+  echo "hangAnalyse.sh [PDB]"
   exit 1 ;
+}
+[ "$1" = "-?" -o "$1" = "-h" ] && usage
+if [ "$1" != "" ]
+then
+  goPDB="alter session set container=$1 ;"
 fi
 file=$(sqlplus / as sysdba <<%% | grep -i "hang analysis in" | sed "s;^.* in ;;"
-alter session set container=$1 ;
+
+$goPDB
+
 oradebug setmypid
 oradebug  unlimit
 oradebug -g all hanganalyze 3
 oradebug -g all dump systemstate 258
 %%
 )
-[ -f $file ] && cat $file || echo "$file not found"
+echo "
+
+=====================================================================================
+
+     Hang analysis done
+
+     Full Trace File is : $file
+
+=====================================================================================
+HANGS from the last analysis
+=====================================================================================
+"
+firstHangOfLastaAnalysis=$(grep -n "HANG ANALYSIS:" $file | tail -1 | cut -f1 -d ":")
+firstHangOfLastaAnalysis=$(($firstHangOfLastaAnalysis - 2))
+echo
+echo "Start line : $firstHangOfLastaAnalysis"
+echo "==========="
+echo
+
+sed -n "$firstHangOfLastaAnalysis,/END OF HANG ANALYSIS/ p" $file
+echo "
+=====================================================================================
+To have all information and dumps, get the
+$file file
+=====================================================================================
+"
